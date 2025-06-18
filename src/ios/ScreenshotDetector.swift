a2d91579e3097359e373d6a1c37dc68ce6e74822
manishg4damco <manishg4@damcogroup.com>
2025-06-18 16:08:10 +0530

    Add files via upload

--- src/ios/ScreenshotDetector.swift
+++ src/ios/ScreenshotDetector.swift
@@ -1,1 +1,21 @@
+import Foundation
+import UIKit
 
+@objc(ScreenshotDetector)
+class ScreenshotDetector: CDVPlugin {
+
+  override func pluginInitialize() {
+    NotificationCenter.default.addObserver(self, selector: #selector(userDidTakeScreenshot), name: UIApplication.userDidTakeScreenshotNotification, object: nil)
+  }
+
+  @objc func userDidTakeScreenshot() {
+    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "screenshot_taken")
+    pluginResult?.setKeepCallbackAs(true)
+    self.commandDelegate.send(pluginResult, callbackId: "screenshot_callback")
+  }
+
+  @objc(startListening:)
+  func startListening(command: CDVInvokedUrlCommand) {
+    self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+  }
+}
