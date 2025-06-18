34540fa9d25aadcf60c88f332d14650c1b9c3335
manishg4damco <manishg4@damcogroup.com>
2025-06-18 16:28:37 +0530

    Update ScreenshotDetector.swift

--- src/ios/ScreenshotDetector.swift
+++ src/ios/ScreenshotDetector.swift
@@ -4,18 +4,26 @@ import UIKit
 @objc(ScreenshotDetector)
 class ScreenshotDetector: CDVPlugin {
 
+  var callbackId: String?
+
   override func pluginInitialize() {
     NotificationCenter.default.addObserver(self, selector: #selector(userDidTakeScreenshot), name: UIApplication.userDidTakeScreenshotNotification, object: nil)
   }
 
   @objc func userDidTakeScreenshot() {
+    guard let callbackId = callbackId else { return }
+
     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "screenshot_taken")
     pluginResult?.setKeepCallbackAs(true)
-    self.commandDelegate.send(pluginResult, callbackId: "screenshot_callback")
+    self.commandDelegate.send(pluginResult, callbackId: callbackId)
   }
 
   @objc(startListening:)
   func startListening(command: CDVInvokedUrlCommand) {
-    self.commandDelegate.send(CDVPluginResult(status: CDVCommandStatus_OK), callbackId: command.callbackId)
+    self.callbackId = command.callbackId
+
+    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "listening_started")
+    pluginResult?.setKeepCallbackAs(true)
+    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
 }
